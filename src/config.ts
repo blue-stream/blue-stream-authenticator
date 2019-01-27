d4eb415a56dc2a5745383188ea1ee3c13cae29e4
Ron Borysovski <roni537@gmail.com>
2019-01-27 17:01:28 +0200

    chore: Authentication and RPC configuration added

--- src/config.ts
+++ src/config.ts
@@ -10,14 +10,37 @@ export const config = {
         persistent: false,
     },
     server: {
-        port:  +(process.env.PORT || 3000),
+        port: +(process.env.PORT || 3000),
         name: 'authentication',
     },
     cors: {
         allowedOrigins: process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : ['http://localhost:4200'],
     },
+    users: {
+        endpoint: process.env.USERS_RPC_ENDPOINT || 'http://localhost',
+        port: +(process.env.USERS_RPC_PORT || 6000),
+        methods: {
+            GET_USER_BY_ID: 'getUserById',
+            CREATE_USER: 'createUser',
+        },
+    },
     authentication: {
         required: true,
         secret: process.env.SECRET_KEY || 'bLue5tream@2018', // Don't use static value in production! remove from source control!
+        saml: {
+            entryPoint: process.env.SAML_ENTRY_POINT || 'http://localhost:8080/simplesaml/saml2/idp/SSOService.php',
+            issuer: process.env.SAML_ISSUER || 'http://localhost:3000/metadata.xml',
+            callbackUrl: process.env.SAML_CALLBACK_URL || 'http://localhost:3000/metadata.xml/callback',
+            authnContext: 'http://schemas.microsoft.com/ws/2008/06/identity/authenticationmethod/windows',
+            identifierFormat: undefined,
+            signatureAlgorithm: 'sha1',
+            acceptedClockSkewMs: -1,
+        },
+        profileExtractor: {
+            id: process.env.PROFILE_EXTRACTOR_ID || 'id',
+            firstName: process.env.PROFILE_EXTRACTOR_FIRST_NAME || 'givenName',
+            lastName: process.env.PROFILE_EXTRACTOR_LAST_NAME || 'surName',
+            mail: process.env.PROFILE_EXTRACTOR_MAIL || 'mail',
+        },
     },
 };
