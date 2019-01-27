9de37f6c0ad00860fae5c894a41d2b5cc6e38df1
Ron Borysovski <roni537@gmail.com>
2019-01-27 17:02:04 +0200

    refactor: Initialize authentication handler

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,8 @@ import * as bodyParser from 'body-parser';
 import * as helmet from 'helmet';
 import * as morgan from 'morgan';
 import { config } from './config';
-import { AppRouter } from './router';
 import { userErrorHandler, serverErrorHandler, unknownErrorHandler } from './utils/errors/errorHandler';
-import { Authenticator } from './utils/authenticator';
+import { AuthenticationController } from './authentication/authentication.handler';
 
 export class Server {
     public app: express.Application;
@@ -19,7 +18,6 @@ export class Server {
     private constructor() {
         this.app = express();
         this.configureMiddlewares();
-        this.app.use(AppRouter);
         this.initializeErrorHandler();
         this.server = http.createServer(this.app);
         this.server.listen(config.server.port, () => {
@@ -54,10 +52,7 @@ export class Server {
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: true }));
 
-        if (config.authentication.required) {
-            this.app.use(Authenticator.initialize());
-            this.app.use(Authenticator.middleware);
-        }
+        AuthenticationController.initialize(this.app);
     }
 
     private initializeErrorHandler() {
