ec8960ff85db510a2bb96507f1f40c8c348d27ac
Ron Borysovski <roni537@gmail.com>
2019-01-27 17:02:37 +0200

    refactor: Remove logger from error handler

--- src/utils/errors/errorHandler.ts
+++ src/utils/errors/errorHandler.ts
@@ -1,15 +1,8 @@
 import * as express from 'express';
 import { ServerError, UserError } from './applicationError';
-import { Logger } from '../logger';
-import { syslogSeverityLevels } from 'llamajs/dist';
 
 export function userErrorHandler(error: Error, req: express.Request, res: express.Response, next: express.NextFunction) {
     if (error instanceof UserError) {
-        Logger.log(
-            syslogSeverityLevels.Notice,
-            'User Error',
-            `${error.name} was thrown with status ${error.status} and message ${error.message}`);
-
         res.status(error.status).send({
             type: error.name,
             message: error.message,
@@ -23,11 +16,6 @@ export function userErrorHandler(error: Error, req: express.Request, res: expres
 
 export function serverErrorHandler(error: Error, req: express.Request, res: express.Response, next: express.NextFunction) {
     if (error instanceof ServerError) {
-        Logger.log(
-            syslogSeverityLevels.Alert,
-            'Server Error',
-            `${error.name} was thrown with status ${error.status} and message ${error.message}`);
-
         res.status(error.status).send({
             type: error.name,
             message: error.message,
@@ -40,11 +28,6 @@ export function serverErrorHandler(error: Error, req: express.Request, res: expr
 }
 
 export function unknownErrorHandler(error: Error, req: express.Request, res: express.Response, next: express.NextFunction) {
-    Logger.log(
-        syslogSeverityLevels.Critical,
-        'Unknown Error',
-        `${error.name} was thrown with status 500 and message ${error.message}`);
-
     res.status(500).send({
         type: error.name,
         message: error.message,
